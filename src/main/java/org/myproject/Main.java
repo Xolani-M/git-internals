1a663d26b0589dc041f59aa27bf592bfa050cb6e
sageMode <nathanielmvana@gmail.com>
2024-12-19 12:59:04 +0200

    Update Main to handle cat-file command and integrate BlobReader

--- src/main/java/org/myproject/Main.java
+++ src/main/java/org/myproject/Main.java
@@ -1,9 +1,11 @@
 package org.myproject;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+
         System.err.println("Program logs: ");
 
         if (args.length < 1) {
@@ -13,16 +15,34 @@ public class Main {
 
         final String command = args[0];
 
-        switch (command){
-            case "init" :
+        switch (command) {
+            case "init" -> {
+                System.err.println("Executing init...");
                 File baseDir = new File(".");
                 try {
                     RepoInitializer.initGitRepo(baseDir);
-                } catch (Exception e){
+                } catch (Exception e) {
                     System.err.println("Error initializing git repo: " + e.getMessage());
                 }
-            default :
-                System.out.println("Unknown command");
+            }
+            case "cat-file" -> {
+                System.err.println("Executing cat-file...");
+                if (args.length != 3) {
+                    System.out.println("Usage: cat-file -p <blob_sha>");
+                    return;
+                }
+                String blobSha = args[2];
+                try {
+                    File baseDir = new File(".");
+                    String content = BlobReader.readBlob(baseDir, blobSha);
+                    System.out.print(content);
+                } catch (IOException e) {
+                    System.err.println("Error reading blob: " + e.getMessage());
+                }
+            }
+            default ->
+                System.out.println("Unknown command: " + command);
+
         }
     }
 }
